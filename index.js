a51bbe1c85c386563a8b9befbb729da664984715
ElliotLuque <elliotluque@gmail.com>
2023-01-15 23:11:11 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const yearInput = document.getElementById("year");
+  const textInput = document.getElementById("text");
+
+  const addButton = document.getElementById("add");
+
+  addButton.addEventListener("click", () => {
+    const year = yearInput.value;
+    const text = textInput.value;
+
+    addCardToTimeline(year, text);
+  });
+});
+
+const createCard = (year, text) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const yearElement = document.createElement("h2");
+  yearElement.innerText = year;
+
+  const textElement = document.createElement("p");
+  textElement.innerText = text;
+
+  card.appendChild(yearElement);
+  card.appendChild(textElement);
+
+  return card;
+};
+
+const addCardToTimeline = (year, text) => {
+  const timelineContainers = document.querySelectorAll(".timeline > *");
+
+  // Create new container
+  const timelineContainer = document.createElement("div");
+  timelineContainer.classList.add("container");
+
+  // Determine if next container is right or left
+  const lastContainer = timelineContainers[0];
+  const lastContainerWasLeft = lastContainer.classList.contains("left");
+
+  lastContainerWasLeft
+    ? timelineContainer.classList.add("right")
+    : timelineContainer.classList.add("left");
+
+  const card = createCard(year, text);
+  timelineContainer.appendChild(card);
+
+  document.querySelector(".timeline").prepend(timelineContainer);
+};
